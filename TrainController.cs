83ad54d17aa2d0d6d1b5552944fb80d2bd5ec0a5
drzzm32 <drzzm32@gmail.com>
2018-05-03 11:05:20 +0800

    update

--- TrainController.cs
+++ TrainController.cs
@@ -5,6 +5,7 @@ namespace NTPUtil
     class TrainController
     {
         public const double DT = 0.001;
+        public const double minV = 0.2;
 
         public static void DoMotionWithAir(TrainPacket packet)
         {
@@ -27,7 +28,36 @@ namespace NTPUtil
 
             if (packet.R < 10)
             {
-                packet.Velocity = Dynamics.LocoMotions.CalcVelocityDownWithAir(Math.Abs(packet.Velocity), 0.1, 1.0, 1.0, 1.0, packet.R / 10.0, DT);
+                double B = Math.Abs(packet.Velocity) < minV ? 2.0 : 1.0;
+                packet.Velocity = Dynamics.LocoMotions.CalcVelocityDownWithAir(Math.Abs(packet.Velocity), 0.1, 1.0, B, 1.0, packet.R / 10.0, DT);
+                if (packet.Velocity < 0.005) packet.Velocity = 0;
+            }
+
+        }
+
+        public static void DoMotionWithAirEx(TrainPacket packet)
+        {
+            if (packet.P > 0 && packet.Velocity < 0.005)
+            {
+                packet.Velocity = 0.005;
+            }
+
+            if (packet.R > 1)
+            {
+                double MaxP = 10.0;
+                double OutP = MaxP / Math.Pow(20.0, Math.E / 2.0) * Math.Pow((double)packet.P, Math.E / 2.0);
+                packet.nextVelocity = Dynamics.LocoMotions.CalcVelocityUpWithAir(Math.Abs(packet.Velocity), 0.1, 1.0, OutP, DT);
+
+                if (packet.Velocity < packet.nextVelocity)
+                {
+                    packet.Velocity = packet.nextVelocity;
+                }
+            }
+
+            if (packet.R < 10)
+            {
+                double B = Math.Abs(packet.Velocity) < minV ? 2.0 : 1.0;
+                packet.Velocity = Dynamics.LocoMotions.CalcVelocityDownWithAir(Math.Abs(packet.Velocity), 0.1, 1.0, B, 1.0, packet.R / 10.0, DT);
                 if (packet.Velocity < 0.005) packet.Velocity = 0;
             }
 
@@ -55,7 +85,8 @@ namespace NTPUtil
 
             if (packet.R < 10)
             {
-                packet.Velocity = Dynamics.LocoMotions.CalcVelocityDownWithAir(Math.Abs(packet.Velocity), 0.1, 1.0, 1.0, 1.0, packet.R / 10.0, DT);
+                double B = Math.Abs(packet.Velocity) < minV ? 2.0 : 1.0;
+                packet.Velocity = Dynamics.LocoMotions.CalcVelocityDownWithAir(Math.Abs(packet.Velocity), 0.1, 1.0, B, 1.0, packet.R / 10.0, DT);
                 if (packet.Velocity < 0.005) packet.Velocity = 0;
             }
 
@@ -85,7 +116,8 @@ namespace NTPUtil
 
             if (packet.R < 10)
             {
-                packet.Velocity = Dynamics.LocoMotions.CalcVelocityDownWithAir(Math.Abs(packet.Velocity), 0.1, 1.0, 1.0, 1.0, packet.R / 10.0, DT);
+                double B = Math.Abs(packet.Velocity) < minV ? 2.0 : 1.0;
+                packet.Velocity = Dynamics.LocoMotions.CalcVelocityDownWithAir(Math.Abs(packet.Velocity), 0.1, 1.0, B, 1.0, packet.R / 10.0, DT);
                 if (packet.Velocity < 0.005) packet.Velocity = 0;
             }
 
